60876642308d0f64fc783779c2dbb26c89c41dcf
MaximaCaceres <maximavanecaceres@gmail.com>
2024-06-13 21:49:39 -0300

    Vectores

---
+++ CargarVector1/CargarVector1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CargarVector1
+{
+    class Program
+    {
+        const int indice = 5;//el indice del vector siempre debe de ser una constante.
+
+        static public int[] vector = new int[indice];//para dar tamaño a nuestro vector, siempre se utiliza la constante de arriba.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese 5 elementos");//le pido ingresar 5 numeros al usuario.
+            TomarElementos();//llamo al metodo para realiar la toma de elementos.
+            Console.Clear();
+            Mostrar();//llamo al metodo para mostrar los elementos que hemos cargado en el vector.
+            Console.ReadKey();
+        }
+        static void TomarElementos()//tomamos los numeros del vector// no es necesario crear una variable para guardarlos y despues pasar al vector.
+        {
+
+            for (int i = 0; i <= indice - 1; i++)
+            {
+                vector[i] = Convert.ToInt16(Console.ReadLine());// con cargaros directamente es suficiente.
+            }
+        }
+        static public void Mostrar()//Metodo para mostrar cada elemento del vector. //Tiene retorno Void porque solo muestra informacion//
+        {
+            Console.WriteLine("Numeros del vector: ");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            for (int i = 0; i <= indice - 1; i++)//al querer rrecorrer un vector, es con -1 ya que sino me salgo de los limites.
+            {
+                Console.Write("| {0} |", vector[i]);//utilice Console.Write sin Line para que se muestren juntos y que no haga salto.
+            }
+        }
+    }
+}
---
+++ CargarVector2/CargarVector2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CargarVector2
+{
+    class Program
+    {
+        const int indice = 100;//indice o limite del vector
+        static public int cont = 0;//con esto voy a saber de cuanto va a ser el vector.
+        static public int[] vector = new int[indice];//vector.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese numeros enteros// -1 para terminar");
+            TomarNumeros();
+            Mostrar();
+            Console.ReadKey();
+        }
+        static public void TomarNumeros()//metodo para tomar los numeros para el vector del cual no se el tamaño.
+        {
+            do//utilizo un do-while ya que no se cuantos numeros van a ser ingresados. 
+            {
+                    vector[cont] = Convert.ToInt16(Console.ReadLine());//cuando tomo el numero, lo hago directo en "cont"
+                    cont++;//este se va a incrementar para luego guardar el otro numero que se ingrese en la segunda posicion o posicion [1]
+            } while (vector[cont-1] != -1 && cont < indice);//aqui digo que se repite cuando cont "ultimo numero ingresado" sea diferente a -1  
+        }//Minetras Vector[cont] sea != -1, sigue. entonces cuando ingreso -1, se guarda en la ultima posicion y al comparar, sale.
+        static public void Mostrar()
+        {
+            Console.WriteLine("numeros del vector: ");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            for (int i = 0; i <cont-1; i++)//aqui digo que voy a mostrar todos los numeros menos el ultimo, ya que ahi se guarda -1
+            {
+                Console.Write("| {0} |", vector[i]);//muestro el elemento del vector en la posicion i que se va a recorrer cont -1 veces.
+            }
+        }
+    }
+}
